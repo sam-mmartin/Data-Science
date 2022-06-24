8d5da3ad6f1189de6b152069a44c0d1cfb724ca0
sam-mmartin <smeirelesmartins@gmail.com>
2022-06-24 14:57:05 -0300

    added seaborn

--- Analisys-and-Visualization/Scripts/aula-01.py
+++ Analisys-and-Visualization/Scripts/aula-01.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 notas = pd.read_csv("Data/ratings.csv")
 print(notas.head())  # retorna as 5 primeiras linhas
@@ -14,8 +15,14 @@ print(notas['nota'])
 print(notas['nota'].unique())
 # retorna a frequencia de cada elemento da serie
 print(notas['nota'].value_counts())
-print(notas['nota'].mean())  # retorna a media dos elementos da serie
+print('Média', notas['nota'].mean())
+print('Mediana', notas.nota.median())
+print(notas.nota.describe())
 
 # plt.plot(notas.nota)
 plt.hist(notas.nota)
+plt.show()
+
+sns.set(style='whitegrid')
+sns.boxplot(x=notas.nota)
 plt.show()
